1a4499dd7e032e1925f1b1f6e517ae89de90371b
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-11-18 22:39:50 -0800

    started working on main page

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableNativeFeedback, Button, Alert, ImageBackground, TouchableOpacity, } from 'react-native';
 
+// import { useDeviceOrientation } from '@react-native-community/hooks;';
+
+const image = require('./assets/HomeBackground.jpg')
 export default function App() {
+  // const orientation = useDeviceOrientation()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // view = div
+    // <SafeAreaView >
+
+    /* <Button
+        color={'red'}
+        title='Add Recipe'
+        // onPress={() => Alert.alert("Add a new recipe?", 'It better be yummy...',
+        //   [{ text: 'yes', onPress: () => console.log('Yes') },
+        //   { text: 'no', onPress: () => console.log('No') }])}
+        onPress={() => Alert.prompt('title', 'message', text => console.log(text))}
+
+      /> */
+
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={image}
+        resizeMode='cover'
+        style={styles.image}>
+        <Text style={styles.titleText}>
+          Hangry
+        </Text>
+      </ImageBackground>
+      <TouchableOpacity
+        style={styles.homeButton}
+        // onPress={handleViewRecipes}
+        >
+        <Text style={styles.buttonText}>View Recipes</Text>
+
+      </TouchableOpacity>
+
+    </SafeAreaView>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 7,
     justifyContent: 'center',
+    position: 'relative'
+  },
+  titleText: {
+    color: 'black',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'lightgray',
+    position: 'absolute',
+    top: 0, // Place the text at the top
+    left: 0, // Center the text horizontally
+    right: 0, // Center the text horizontally
+  },
+  homeButton: {
+    flex: 0.5,
+    color: 'dodgerblue',
+    lineHeight: 84,
+    textAlign: 'center',
+    backgroundColor: 'dodgerblue',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    alignContent: 'center'
   },
 });
